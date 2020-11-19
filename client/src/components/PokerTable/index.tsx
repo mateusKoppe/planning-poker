2c597ffabdd67dc58798cbadbdd95b9871dd98da
Mateus Koppe <mateus.koppe@gmail.com>
2020-11-19 17:53:31 -0300

    Implement typescript

---
+++ client/src/components/PokerTable/index.tsx
@@ -0,0 +1,28 @@
+import React, { ReactNode } from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  border: .8em solid ${({ theme }) => theme.colors.pokerBorderDark};
+  border-radius: 3.75rem;
+`
+
+const Content = styled.div`
+  border-radius: 3rem;
+  padding: 2em;
+  background-color: ${({ theme }) => theme.colors.pokerGreen};
+  border: .7em solid ${({ theme }) => theme.colors.pokerBorderLight};
+`
+
+type PokerTableProps = {
+  children: ReactNode
+}
+
+const PokerTable = ({ children }: PokerTableProps) => {
+  return (
+    <Wrapper>
+      <Content>{children}</Content>
+    </Wrapper>
+  );
+};
+
+export default PokerTable;
--- client/src/utils/styles/theme.ts
+++ client/src/utils/styles/theme.ts
@@ -1,4 +1,23 @@
-const theme = {
+import { DefaultTheme } from 'styled-components'
+
+declare module 'styled-components' {
+  interface DefaultTheme {
+    colors: {
+      main: string,
+      pokerGreen: string,
+      pokerGreenDark: string,
+      pokerBorderDark: string,
+      pokerBorderLight: string,
+      pokerBackground: string,
+    },
+    fonts: {
+      family: string,
+      size: string
+    }
+  }
+}
+
+const theme: DefaultTheme  = {
   colors: {
     main: "#17A65A",
     pokerGreen: "#009345",
